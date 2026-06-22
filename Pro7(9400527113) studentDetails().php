e3ff19f2b130d97fce47f57f541279d0a5980c8b
Rishiraj7113 <parmar.rishiraj2006@gmail.com>
2026-06-22 09:39:54 +0530

    Add files via upload

---
+++ Pro7(9400527113) studentDetails().php
@@ -0,0 +1,12 @@
+<?php
+
+function studentDetails($name, $enrollment, $semester)
+{
+    echo "Student Name: " . $name . "<br>";
+    echo "Enrollment Number: " . $enrollment . "<br>";
+    echo "Semester: " . $semester . "<br>";
+}
+
+studentDetails("Parmar Rishiraj", "92400527113", "Semester 5");
+
+?>
